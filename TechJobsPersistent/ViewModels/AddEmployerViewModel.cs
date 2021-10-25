ddbcf0a759db1a50db4843ab6c14b380f92bdbd8
Donna Seidel <dseidel01@gmail.com>
2021-10-24 19:19:41 -0500

    Add EmployerviewModel and AddJobViewModel Update HomeController to handle Add() Update AddJob.cshtml to show List of Employers and link to Add Employer

--- TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -9,8 +9,6 @@ namespace TechJobsPersistent.ViewModels
 {
     public class AddEmployerViewModel
     {
-
-
         [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Location is required")]
---
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobsPersistent.Models;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddJobViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
+        public string Name { get; set; } //Job Name
+
+        public List<SelectListItem> Employers { get; set; }
+        public int EmployerId { get; set; }
+
+        public AddJobViewModel(List<Employer> employers)
+        {
+            Employers = new List<SelectListItem>();
+
+            foreach (var employer in employers)
+            {
+                Employers.Add(
+                    new SelectListItem
+                    {
+                        Value = employer.Id.ToString(),
+                        Text = employer.Name
+                    }
+                ); ;
+            }
+        }
+        public AddJobViewModel() { }
+    }
+}
