a2c73cab12686132aab316854c9096594461f0aa
Donna Seidel <dseidel01@gmail.com>
2021-10-29 14:32:38 -0500

    add label clarity to addjob view include query only for skills that have a job listed

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -5,4 +5,9 @@ SELECT DATA_TYPE from INFORMATION_SCHEMA.COLUMNS where table_schema = 'techjobs'
 SELECT * FROM techjobs.employers where location = 'Saint Louis' order by Name;
 
 --Part 3
+select distinct s.Name, s.Description from skills s
+join jobskills js on s.Id = js.SkillId
+left join jobs j on j.Id = js.JobId
+where js.jobid is not null
+order by s.Name;
 
